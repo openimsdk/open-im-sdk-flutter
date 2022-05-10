3cb2c6d51430ef8984f1d8b4a4823ac29a9a1ec1
std-s <278779615@qq.com>
2022-05-10 15:02:18 +0800

    [Fix] object is NSNull.

--- ios/Classes/JsonUtil.swift
+++ ios/Classes/JsonUtil.swift
@@ -3,9 +3,15 @@ import Foundation
 public class JsonUtil {
     
     public static func toString(object: AnyObject?)->String {
+        
+        if object is NSNull {
+            return ""
+        }
+        
         if object == nil {
             return ""
         }
+        
         if object is String{
             let s = object as! String
             let b = s.replacingOccurrences(of: "\"", with: "\\\"")
