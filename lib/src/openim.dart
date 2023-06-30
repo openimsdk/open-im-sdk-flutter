79cbae718ce195ef933f9d7b0acdceb07a86d804
hrxiang <xianghairui@126.com>
2023-06-30 18:26:50 +0800

    open 3.0

--- lib/src/openim.dart
+++ lib/src/openim.dart
@@ -2,7 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_openim_sdk/flutter_openim_sdk.dart';
 
 class OpenIM {
-  static const version = '2.3.5+4';
+  static const version = '3.0.0';
 
   static const _channel = const MethodChannel('flutter_openim_sdk');
 
